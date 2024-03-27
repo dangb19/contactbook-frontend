c812db929c352a21bb5de66055279a9ea3c05194
lenhutminhdang <ginoace2k1@gmail.com>
2024-03-27 12:31:06 +0700

    Tao trang them moi lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 import NotFound from "@/views/NotFound.vue";
 import ContactEdit from "@/views/ContactEdit.vue";
+import ContactCreate from "@/views/ContactCreate.vue";
 
 const routes = [
   {
@@ -18,6 +19,12 @@ const routes = [
     path: "/contacts/:id",
     name: "contact.edit",
     component: ContactEdit,
+    props: true,
+  },
+  {
+    path: "/contacts/create",
+    name: "contact.add",
+    component: ContactCreate,
     props: true,
   },
 ];
